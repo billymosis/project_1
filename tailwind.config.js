c887ecbb12a495fa471ea1258328d795783ecb50
Billy Mosis <billy.mosis@majoo.id>
2022-05-19 20:23:35 +0700

    update baru beli domain

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   mode: "jit",
-  purge: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue,html}"],
+  content: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue,html}"],
   plugins: [require("@tailwindcss/typography")],
 };
